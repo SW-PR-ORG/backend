973382a57448312b428fc9407872dc33367cd333
Omar Ameen <omarmohamedameen@outlook.com>
2025-12-22 22:11:40 +0200

    search ready

---
+++ is_leaked.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+df = pd.read_csv("E:/New SW PR/backend/Ignored datasets/rockyou_dataset_cleaned.csv", usecols=['password'])
+
+password_set = set(df['password'].astype(str))
+
+def password_exists(password):
+
+    return password in password_set
+
+ 
